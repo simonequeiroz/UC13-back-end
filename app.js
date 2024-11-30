e1ab5da186f533ab4638b15c1010c2a158664db0
Simone <sqb1410@gmail.com>
2024-11-29 22:06:19 -0300

    Aula

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import tabelacarro from './tabelacarros.js';
+import tabelacarros from './tabelacarros.js';
 import express from 'express';
  
 const app = express();
@@ -6,10 +6,33 @@ const app = express();
 app.use(express.json());
  
 app.get( '/', (requisicao, resposta) => {
-    resposta.status(200).send(tabelacarro);
+    resposta.status(200).send(tabelacarros);
 });
 
-// Inicia o servidor na porta 3000:
+app.get('/:sigla', (requisiçao, resposta)  => {
+    const siglaInformada = requisicao.params.sigla.toUpperCase(); // Obtendo a Sigla
+    const carro = tabelacarros.find((infoCarro) => infoCarro.sigla === siglaInformada); // Buscando o carro pela sigla
+    if (!carro) {
+        // ! Vazio Not
+        // Se o carro não for encontrado, retorna erro 404
+        resposta
+        .status(404)
+        .send(
+            'não existe um carro com a sigla informada!'  // Mensagem de erro
+        );
+        return;
+    }
+    resposta.status(200).send(carro); // Se encontrado a sigla retorna a resposta correta 
+
+});
+app.post('/', (req, res) => {
+    const novoCarro = req.body; // Obtem o corpo enviado para incluir um carro
+    tabelacarros.push(novoCarro); //Adiciona o novo carro a lista de carros
+    res.status(200).send(novoCarro); // Retorna o carro adicionado com status 200(ok)
+});
+
+    
+    // Inicia o servidor na porta 3000:
 app.listen(3000,() => console.log("Servidor Rodando com Sucesso"));
  
 // node app.js
--- tabelacarros.js
+++ tabelacarros.js
@@ -1,4 +1,3 @@
-// Dados dos carros
 const tabelacarros = [
     {
         nome: "Ferrari",
@@ -17,7 +16,44 @@ const tabelacarros = [
         consumo: "5 km/l",
         aceleracao: "2.8 s (0-100 km/h)",
         preco: "R$ 3.500.000"
+    },
+    {
+        nome: "Fusca",
+        sigla: "VW-F",
+        velocidadeMaxima: "140 km/h",
+        potencia: "60 CV",
+        consumo: "12 km/l",
+        aceleracao: "0-100 km/h em 15 s",
+        preco: "R$ 30.000"
+    },
+    {
+        nome: "Corolla",
+        sigla: "TOY-C",
+        velocidadeMaxima: "200 km/h",
+        potencia: "177 CV",
+        consumo: "14 km/l",
+        aceleracao: "0-100 km/h em 9 s",
+        preco: "R$ 140.000"
+    },
+    {
+        nome: "Mustang",
+        sigla: "FORD-M",
+        velocidadeMaxima: "250 km/h",
+        potencia: "450 CV",
+        consumo: "7 km/l",
+        aceleracao: "0-100 km/h em 4.5 s",
+        preco: "R$ 400.000"
+    },
+    {
+        nome: "Camaro",
+        sigla: "CHEV-CAM",
+        velocidadeMaxima: "290 km/h",
+        potencia: "455 CV",
+        consumo: "6 km/l",
+        aceleracao: "0-100 km/h em 4 s",
+        preco: "R$ 450.000"
     }
 ];
 
-export default (tabelacarros);
+// Exportando corretamente
+export default tabelacarros;
