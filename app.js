4e3d21f2cb0b049fc35bf1f1a5d7047ad5b1bdb9
Simone <sqb1410@gmail.com>
2024-12-03 22:05:40 -0300

    Aula0312

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const app = express();
  
 app.use(express.json());
  
-app.get( '/', (requisicao, resposta) => {
+app.get('/', (requisicao, resposta) => {
     resposta.status(200).send(tabelacarros);
 });
 
 app.get('/:sigla', (requisiçao, resposta)  => {
-    const siglaInformada = requisicao.params.sigla.toUpperCase(); // Obtendo a Sigla
+    const siglaInformada = req.params.sigla.toUpperCase(); // Obtendo a Sigla
     const carro = tabelacarros.find((infoCarro) => infoCarro.sigla === siglaInformada); // Buscando o carro pela sigla
     if (!carro) {
         // ! Vazio Not
@@ -32,8 +32,43 @@ app.post('/', (req, res) => {
 });
 
     
-    // Inicia o servidor na porta 3000:
-app.listen(3000,() => console.log("Servidor Rodando com Sucesso"));
- 
-// node app.js
+app.put('/:sigla', (req, res) => {
+    const siglaInformada = req.params.sigla.toUpperCase();
+    const carroSelecionado = tabelacarros.find(c => c.sigla === siglaInformada);
+    if (!carroSelecionado) {
+        res
+        .status(404)
+        .send( 
+            'não existe um carro com a sigla informada!' //Mensagem de erro
+                );
+        return;
+        };
+        const campos = Object.keys(req.body);
+        for (let campo of campos) {
+            carroSelecionado[campos] = req.body[campos];
+        }
+        res.status(200).send(carroSelecionado);
+    });
+    app.delete('/:sigla', (req, res) => {
+        const siglaInformada = req.params.sigla.toUpperCase(); // Obtem a sigla
+        const IndiceCarroSelecionado = tabelacarros.findIndex(
+            (c) => c.sigla === siglaInformada // Busca o indice do carro na lista 
+        );
+        if (IndiceCarroSelecionado === -1) {
+            //Se o carro não for encontrado/indice retorna -1
+            res
+            .status(404)
+            .send(
+                'Não existe um carro com a sigla informada!' // Mensagem de erro
+
+            );
+        return;
+        };
+        const carroRemovido = tabelacarros.splice(IndiceCarroSelecionado, 1); //Remove o carro 
+        res.status(200).send(carroRemovido); //Retorna o carro removido com ok
+    });
+
+// Inicia o servidor na porta 3000:
+    app.listen(3000,() => console.log("Servidor Rodando com Sucesso"));
+ // node app.js
 // localhost:3000/
