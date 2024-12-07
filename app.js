2a232a89d16f1e455e725ff62b4fdcff1ce7576e
Simone <sqb1410@gmail.com>
2024-12-06 22:11:08 -0300

    Aula0612

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 import tabelacarros from './tabelacarros.js';
 import express from 'express';
+
+import {modeloCarro, modeloAtualizacaoCarro} from './validacao.js';
  
 const app = express();
  
 app.use(express.json());
  
-app.get('/', (requisicao, resposta) => {
+app.get('/', (req, res) => {
     resposta.status(200).send(tabelacarros);
 });
 
-app.get('/:sigla', (requisiçao, resposta)  => {
+app.get('/:sigla', (req, res)  => {
     const siglaInformada = req.params.sigla.toUpperCase(); // Obtendo a Sigla
     const carro = tabelacarros.find((infoCarro) => infoCarro.sigla === siglaInformada); // Buscando o carro pela sigla
     if (!carro) {
@@ -27,10 +29,16 @@ app.get('/:sigla', (requisiçao, resposta)  => {
 });
 app.post('/', (req, res) => {
     const novoCarro = req.body; // Obtem o corpo enviado para incluir um carro
+    // JoI
+    const { error } = modeloCarro.validate(novoCarro);
+    if (error) {
+        // Se houver erro retorna erro 400 (Bad Request)
+        res.status(400).send(error);
+        return;
+    }
     tabelacarros.push(novoCarro); //Adiciona o novo carro a lista de carros
-    res.status(200).send(novoCarro); // Retorna o carro adicionado com status 200(ok)
+    res.status(201).send(novoCarro); // Retorna o carro adicionado com status 200(ok)
 });
-
     
 app.put('/:sigla', (req, res) => {
     const siglaInformada = req.params.sigla.toUpperCase();
@@ -43,9 +51,16 @@ app.put('/:sigla', (req, res) => {
                 );
         return;
         };
-        const campos = Object.keys(req.body);
+        // JOI
+        const {error} = modeloAtualizacaoCarro.validate(req.body);
+        if (error) {
+            //Se houver erro no modelo/validação retorna erro
+            res.status(400).send(error);
+            return;
+            }
+        const campos = Object.keys(req.body); // Obtem o corpo da requisição enviada
         for (let campo of campos) {
-            carroSelecionado[campos] = req.body[campos];
+            carroSelecionado[campos] = req.body[campos]; //Atualiza o carro com a informação
         }
         res.status(200).send(carroSelecionado);
     });
